211996d167a53f61d5612f680477bcbba014e700
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-09 11:09:09 +0100

    feat: make connexion to database

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 8080
+require("dotenv").config();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+//CONNEXION TO DATABASE
+const connex = mysql.createPool({
+	connectionLimit: 10,
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database : process.env.DB_DATABASE,
+});
+connex.getConnection((err) => {
+  if (err) {
+    console.error('Une erreur detectée : ' + err);
+    return
+  }
+  else {
+    console.log("Connecté à la base de données MySQL!"); 
+  }
+})
+
+
 
 //SERVEUR
-app.listen(port, () => {
-  console.log(`le serveur tourne au port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`le serveur tourne au port ${process.env.PORT}`)
 })
