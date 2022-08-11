a63fe0fcefc24272deaf810da1f7ff78f5ecbd12
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-11 11:03:07 +0100

    feat: import bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bcrypt = require('bcrypt');
 const mysql = require('mysql');
 const app = express();
 /////////////////////////////////////////////////////////////////////////////////////////
